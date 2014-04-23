84416e513d51f8d2b3f85eba11f53f2a0f7035e7
James Lan <jameslan@gmail.com>
2014-04-22 22:20:14 -0700

    Add empty Sample and Tests projects

---
+++ ArgParser.NET.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgParser.NET.Sample
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var parser = new ArgParser {};
+            var extra = parser.Parse(args);
+        }
+    }
+}
