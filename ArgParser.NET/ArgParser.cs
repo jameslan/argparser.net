fee4a456979876e553c7d936d476fd503b8cd578
James Lan <jameslan@gmail.com>
2014-04-22 22:36:39 -0700

    Add a do-nothing method Parse() to ArgParser so it passes build.

--- ArgParser.NET/ArgParser.cs
+++ ArgParser.NET/ArgParser.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace ArgParser.NET {
     public class ArgParser {
+        public List<string> Parse(string[] args) {
+            var extra = new List<string>();
+            return extra;
+        }
     }
 }
